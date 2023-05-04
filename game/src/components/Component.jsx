0a82a7874cb602d991df3f6dd10a523d4afee929
David_Hernandez <julio-david@hotmail.com>
2023-05-04 17:09:19 +0000

    Start React APP

---
+++ game/src/components/Component.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export class Basic extends Component {
+  render (){
+
+    return (
+      <div> 
+        <input type="text" /> 
+        <button> Update </button>
+        <p>Text input</p>
+        <p>Text updated</p>
+        </div>
+    )
+  }
+}
+
+export default Basic;
