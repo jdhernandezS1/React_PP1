23912d82ffc3d909528943cbb40d4d8cccfe3a98
David_Hernandez <julio-david@hotmail.com>
2023-05-05 16:06:06 +0000

    change name component and title

--- game/src/index.js
+++ game/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import { Basic } from './components/Component';
+import { GridMovies } from './components/Component';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Basic />
+    <GridMovies />
   </React.StrictMode>
 );
 
