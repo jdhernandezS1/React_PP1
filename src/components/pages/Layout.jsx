0ee4f0f05cd6bb424ade05d929300620536f068b
David_Hernandez <julio-david@hotmail.com>
2023-05-09 10:09:18 +0000

    Fixing React Router library dependencies

--- src/components/pages/Layout.jsx
+++ src/components/pages/Layout.jsx
@@ -9,6 +9,7 @@ const Layout = () => {
   return (
     <>
 
+      <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
       <div className={navPc}>
         <NavBarPc />
       </div>
