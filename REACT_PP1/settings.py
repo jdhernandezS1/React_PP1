6e9a4b112c52fb1a2092acae460c072d87874f46
David_Hernandez <julio-david@hotmail.com>
2023-05-09 21:44:32 +0000

    deploy Heroku dir

--- REACT_PP1/settings.py
+++ REACT_PP1/settings.py
@@ -60,11 +60,27 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'REACT_PP1.urls'
 
+# if USE_S3:
+#     # aws settings
+#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+#     AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     # s3 static settings
+#     AWS_LOCATION = 'build'
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#     dir = STATIC_URL
+# else:
+dir = os.path.join(BASE_DIR, 'game/build')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'game/build')
+            dir
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -129,8 +145,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
 if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
