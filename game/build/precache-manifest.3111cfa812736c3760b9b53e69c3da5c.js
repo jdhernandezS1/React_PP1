5971359ebb531b1145446d079c218af5447ca732
David_Hernandez <julio-david@hotmail.com>
2023-05-09 18:18:49 +0000

    Django and React Conection

---
+++ game/build/precache-manifest.3111cfa812736c3760b9b53e69c3da5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "480d3068aa6d5f30c35b",
+    "url": "/static/css/main.07911f68.chunk.css"
+  },
+  {
+    "revision": "480d3068aa6d5f30c35b",
+    "url": "/static/js/main.43b9b498.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4e2fab6f1aaea75c815",
+    "url": "/static/js/2.8a4b6b7d.chunk.js"
+  },
+  {
+    "revision": "4f93f31391a6e51afc003bf1207bc113",
+    "url": "/index.html"
+  }
+];
