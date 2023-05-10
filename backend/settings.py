05be7fe8f2d7485803fa17de181b7e127546407f
David_Hernandez <julio-david@hotmail.com>
2023-05-10 21:57:18 +0000

    Re build backend

--- backend/settings.py
+++ backend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-USE_S3 = os.getenv('USE_S3') == 'TRUE'
+USE_S3 = os.environ.get('USE_S3') == 'TRUE'
 if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
